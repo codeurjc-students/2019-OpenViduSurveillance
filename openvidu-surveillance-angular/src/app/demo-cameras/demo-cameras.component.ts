f2ee6b6793b3443d3911ed6cbdc2e644833f2249
Igort142 <igort142@gmail.com>
2020-04-27 16:39:57 +0200

    PTZ

--- openvidu-surveillance-angular/src/app/demo-cameras/demo-cameras.component.ts
+++ openvidu-surveillance-angular/src/app/demo-cameras/demo-cameras.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CameraService} from '../camera.service';
+// import { onvifDevices$ } from 'camera-probe'
 
 @Component({
   selector: 'app-demo-cameras',
@@ -16,4 +17,26 @@ export class DemoCamerasComponent implements OnInit {
   addDemoCameras() {
     this.cameraService.publishDemoCameras(this.sessionId);
   }
+
+  // discoverCameras() {
+  //   const subscription = onvifDevices$().subscribe(console.log)
+  // }
+//   discoverCameras() {
+//     const onvif = require('node-onvif');
+//
+//     console.log('Start the discovery process.');
+// // Find the ONVIF network cameras.
+// // It will take about 3 seconds.
+//     onvif.startProbe().then((device_info_list) => {
+//       console.log(device_info_list.length + ' devices were found.');
+// // Show the device name and the URL of the end point.
+//       device_info_list.forEach((info) => {
+//         console.log('- ' + info.urn);
+//         console.log('  - ' + info.name);
+//         console.log('  - ' + info.xaddrs[0]);
+//       });
+//     }).catch((error) => {
+//       console.error(error);
+//     });
+//   }
 }
