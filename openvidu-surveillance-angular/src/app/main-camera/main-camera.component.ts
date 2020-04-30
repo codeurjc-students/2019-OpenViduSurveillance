41fbfa8dc5ef7afa16e876028718d0651b45b97f
Igort142 <igort142@gmail.com>
2020-04-30 11:57:31 +0200

    Added PTZ control icons for the frontend

--- openvidu-surveillance-angular/src/app/main-camera/main-camera.component.ts
+++ openvidu-surveillance-angular/src/app/main-camera/main-camera.component.ts
@@ -1,18 +1,45 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {StreamManager} from 'openvidu-browser';
 import {CameraService} from '../camera.service';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {resolve} from "@angular/compiler-cli/src/ngtsc/file_system";
 
 @Component({
-  selector: 'app-main-camera',
-  templateUrl: './main-camera.component.html',
-  styleUrls: ['./main-camera.component.css']
+    selector: 'app-main-camera',
+    templateUrl: './main-camera.component.html',
+    styleUrls: ['./main-camera.component.css']
 })
 export class MainCameraComponent implements OnInit {
-  @Input() mainStreamManager: StreamManager;
+    @Input() mainStreamManager: StreamManager;
+    ptz: Boolean = false;
+    ipUrl: String;
 
-  constructor(private cameraService: CameraService) { }
+    constructor(private cameraService: CameraService,
+                private httpClient: HttpClient) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    move(direction: string) {
+        this.ipUrl = this.mainStreamManager.stream.connection.data;
+        let formData = new FormData();
+        formData.append('user', 'admin');
+        formData.append('password', 'admin');
+        this.httpClient.post('https://localhost:8080/ptz/' + this.ipUrl + '/' + direction, formData, {responseType: 'text'}).subscribe(res => {
+            console.log(JSON.parse(res));
+        })
+    }
+
+    ptzAvailable() {
+        this.ipUrl = this.mainStreamManager.stream.connection.data;
+        let formData = new FormData();
+        formData.append('user', 'admin');
+        formData.append('password', 'admin');
+        console.log(this.ipUrl);
+        this.httpClient.post('https://localhost:8080/ptz/' + this.ipUrl, formData, {responseType: 'text'}).subscribe(res => {
+            this.ptz = JSON.parse(res);
+        })
+    }
 
 }
