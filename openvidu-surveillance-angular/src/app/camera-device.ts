d5a6b52d1fd65d6d3f243c3bda841df3bbb385f0
Igort142 <igort142@gmail.com>
2020-04-30 11:56:02 +0200

    New functionality, local network cameras
    You may scan your local network in order to get the cameras connected to it, then they will appear in a dropdown on the frontpage, from there you can choose one and add it.

---
+++ openvidu-surveillance-angular/src/app/camera-device.ts
@@ -0,0 +1,6 @@
+export interface CameraDevice {
+    id: number;
+    session: string;
+    url: string;
+    camera: string;
+}
--- openvidu-surveillance-angular/src/app/camera.service.ts
+++ openvidu-surveillance-angular/src/app/camera.service.ts
@@ -1,66 +1,93 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError as observableThrowError} from 'rxjs/internal/observable/throwError';
 import {StreamManager} from 'openvidu-browser';
+import {CameraDevice} from './camera-device';
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CameraService {
-  private OPENVIDU_SERVER_SECRET = 'MY_SECRET';
-  private OPENVIDU_SERVER_URL = 'https://localhost:8080/';
-  public mainStreamManager: StreamManager;
-  constructor(public httpClient: HttpClient) { }
+    private OPENVIDU_SERVER_SECRET = 'MY_SECRET';
+    private OPENVIDU_SERVER_URL = 'https://localhost:8080/';
+    public mainStreamManager: StreamManager;
+    public availableIpCameras: CameraDevice[];
 
-  publishDemoCameras(mySessionId: string) {
-    const options = {
-      headers: new HttpHeaders({
-        'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + this.OPENVIDU_SERVER_SECRET),
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*'
-      })
-    };
-    this.httpClient.post('https://localhost:8080/addDemoCameras', mySessionId, options).subscribe(result => {
-      console.log(result);
-    });
-  }
-  publishIpCamera(sessionId, rtspUri, cameraName, adaptativeBitrate, onlyPlayWhenSubscribers) {
-    return new Promise((resolve, reject) => {
+    constructor(public httpClient: HttpClient) {
+        this.addLocalCameras();
+    }
 
-      const body = JSON.stringify({
-        rtspUri: rtspUri, data: cameraName, adaptativeBitrate: adaptativeBitrate,
-        onlyPlayWithSubscribers: onlyPlayWhenSubscribers
-      });
-      const options = {
-        headers: new HttpHeaders({
-          'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + this.OPENVIDU_SERVER_SECRET),
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        })
-      };
-      // return this.httpClient.post(this.OPENVIDU_SERVER_URL + '/api/sessions/' + sessionId + '/connection', body, options)
-      return this.httpClient.post('https://localhost:8080/session/' + sessionId + '/addIpCamera', body, options)
-          .pipe(
-              catchError(error => {
-                reject(error)
-                if (error.status === 409) {
-                  resolve(sessionId);
-                } else {
-                  console.warn('No connection to OpenVidu Server. This may be a certificate error at ' + this.OPENVIDU_SERVER_URL);
-                  if (window.confirm('No connection to OpenVidu Server. This may be a certificate error at \"' + this.OPENVIDU_SERVER_URL +
-                      '\"\n\nClick OK to navigate and accept it. If no certificate warning is shown, then check that your OpenVidu Server' +
-                      'is up and running at "' + this.OPENVIDU_SERVER_URL + '"')) {
-                    location.assign(this.OPENVIDU_SERVER_URL + '/accept-certificate');
-                  }
-                }
-                return observableThrowError(error);
-              })
-          )
-          .subscribe(response => {
+
+    publishDemoCameras(mySessionId: string) {
+        const options = {
+            headers: new HttpHeaders({
+                'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + this.OPENVIDU_SERVER_SECRET),
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*'
+            })
+        };
+        this.httpClient.post('https://localhost:8080/addDemoCameras', mySessionId, options).subscribe(result => {
+            console.log(result);
+        });
+    }
+
+    publishIpCamera(sessionId, rtspUri, cameraName, adaptativeBitrate, onlyPlayWhenSubscribers) {
+        return new Promise((resolve, reject) => {
+            console.log('Camera ' + rtspUri);
+            const body = JSON.stringify({
+                rtspUri: rtspUri, data: cameraName, adaptativeBitrate: adaptativeBitrate,
+                onlyPlayWithSubscribers: onlyPlayWhenSubscribers
+            });
+            const options = {
+                headers: new HttpHeaders({
+                    'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + this.OPENVIDU_SERVER_SECRET),
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                })
+            };
+            // return this.httpClient.post(this.OPENVIDU_SERVER_URL + '/api/sessions/' + sessionId + '/connection', body, options)
+            return this.httpClient.post('https://localhost:8080/session/' + sessionId + '/addIpCamera', body, options)
+                .pipe(
+                    catchError(error => {
+                        reject(error)
+                        if (error.status === 409) {
+                            resolve(sessionId);
+                        } else {
+                            console.warn('No connection to OpenVidu Server. This may be a certificate error at ' + this.OPENVIDU_SERVER_URL);
+                            if (window.confirm('No connection to OpenVidu Server. This may be a certificate error at \"' + this.OPENVIDU_SERVER_URL +
+                                '\"\n\nClick OK to navigate and accept it. If no certificate warning is shown, then check that your OpenVidu Server' +
+                                'is up and running at "' + this.OPENVIDU_SERVER_URL + '"')) {
+                                location.assign(this.OPENVIDU_SERVER_URL + '/accept-certificate');
+                            }
+                        }
+                        return observableThrowError(error);
+                    })
+                )
+                .subscribe(response => {
+                    console.log(response);
+                    resolve(response['id']);
+                });
+        });
+    }
+
+    discoverCameras(sessionID) {
+        const options = {
+            headers: new HttpHeaders({
+                'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + this.OPENVIDU_SERVER_SECRET),
+                'Content-Type': 'text/plain'
+            })
+        };
+        this.httpClient.post('https://localhost:8080/discover', sessionID, options).subscribe(response => {
             console.log(response);
-            resolve(response['id']);
-          });
-    });
-  }
+        });
+    }
+    addLocalCameras() {
+        this.httpClient.get('https://localhost:8080/localCameras', {responseType: 'text'}).subscribe(response => {
+            console.log(response);
+            console.log(JSON.parse(response));
+            this.availableIpCameras = JSON.parse(response);
+        })
+    }
 }
