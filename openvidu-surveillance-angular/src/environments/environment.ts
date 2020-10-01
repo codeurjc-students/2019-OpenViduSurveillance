48a5b7dd11e05b5317778f6f4cc58e37719360aa
Igort142 <igort142@gmail.com>
2020-10-01 21:14:44 +0200

    Docker compose and docker file
    The app is now ready to deploy as a docker container using docker compose

--- openvidu-surveillance-angular/src/environments/environment.ts
+++ openvidu-surveillance-angular/src/environments/environment.ts
@@ -2,7 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
-
+//  OPENVIDUSURVEILLANCE_BACKEND_URL: 'https://localhost:8080/'
 export const environment = {
-  production: false
+  production: false,
+  OPENVIDUSURVEILLANCE_BACKEND_URL: 'https://localhost:8080/'
 };
