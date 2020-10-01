43925653076793d08aaf393f4a0bd468a92b6aea
Igort142 <igort142@gmail.com>
2020-10-01 21:22:19 +0200

    Project reestructure

--- OpenViduBackend/src/main/java/com/example/com/example/error/CameraAlreadyInSessionException.java
+++
@@ -1,7 +0,0 @@
-package com.example.com.example.error;
-
-public class CameraAlreadyInSessionException extends Exception {
-    public CameraAlreadyInSessionException(String message) {
-        super(message);
-    }
-}
